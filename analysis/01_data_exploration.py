018b7c9830aabb8f757ec9304ec5685e29b22fe3
ishaq231 <ishaq.mushtaq06@gmail.com>
2025-11-01 16:50:07 +0000

    CHORE: analysing csv file

--- analysis/01_data_exploration.py
+++ analysis/01_data_exploration.py
@@ -1,1 +1,26 @@
 #Test File for Data Exploration
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_file(file_path):
+    data = pd.read_csv(file_path)
+    return data
+
+def data_clean(data):
+    data.dropna(inplace=True)
+    data.drop_duplicates(inplace=True)
+    return data
+def plot_data(data):
+    headers = data.columns.tolist()
+    data.plot(x=headers[0], kind='line')
+    plt.ylabel('Number Sold')
+    plt.title('Sales Data Over Time')
+    plt.show()
+    
+
+
+
+
+drinks_data = pd.read_csv('data/raw/Pink CoffeeSales March - Oct 2025.csv')
+drinks_clean_data = data_clean(drinks_data)
+plot_data(drinks_clean_data)
